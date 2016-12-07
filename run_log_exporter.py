ec49461d0cf6d2dae3cedc7bf6f6eea7f4a8756f
salwator <salwator@gmail.com>
2016-12-07 16:27:35 +0100

    more verbose communication

--- run_log_exporter.py
+++ run_log_exporter.py
@@ -28,23 +28,34 @@ def workout_ids(session, num_of_pages):
         workouts = re.findall('show_workout\(\d+', page_code.text)
         return [extract_id(workout_str) for workout_str in workouts]
     def get_ids(pages):
+        print("Fetching workouts from {} pages.".format(pages))
         return [ids_from_page(page_num) for page_num in range(1, pages + 1)]
     return list(chain(*get_ids(num_of_pages)))
 
 
+def get_id(page):
+    id = re.findall('wt_id&quot;: \d+', page.text)[0]
+    return id.replace('wt_id&quot;: ', '')
+
+
+def get_date(page):
+    date = re.findall('Data:</span><span class="value">\d+-\d+-\d+', page.text)[0]
+    return date.replace('Data:</span><span class="value">', '')
+
+
 def gpx_ids(session, workouts):
-    k = []
+    print("Fetching workouts in search for gpx ids.")
+    ids = []
     for count, workout_id in enumerate(workouts):
-        q = session.get('https://run-log.com/workout/workout_show/{}'.format(workout_id))
+        page_code = session.get('https://run-log.com/workout/workout_show/{}'.format(workout_id))
         print('{}/{}'.format(count + 1, len(workouts)))
         try:
-            z = re.findall('wt_id&quot;: \d+', q.text)[0]
-            c = re.findall('Data:</span><span class="value">\d+-\d+-\d+', q.text)[0]
-            k.append((z.replace('wt_id&quot;: ', ''), c.replace('Data:</span><span class="value">', '')))
+            workout = get_id(page_code), get_date(page_code)
+            ids.append(workout)
         except:
-            print('No gpx!')
+            print('No gpx found for workout id {}!'.format(workout_id))
             pass
-    return k
+    return ids
 
 
 def save_gpx(gpx, id, day):
@@ -60,12 +71,12 @@ def download_gpxies(session, ids):
     def fill_activity_type(gpx):
         return re.sub('<trk><trkseg>', '<trk><type>RUNNING</type><trkseg>', gpx)
     for count, (id, day) in enumerate(ids):
+        print("Downloading gpx: {} {}/{}".format(id, count + 1, len(ids)))
         gpx = fill_activity_type(correct_dates(get_gpx(id)))
         save_gpx(gpx, id, day)
-        print("{}/{}".format(count + 1, len(ids)))
 
 
 run_log_session = open_run_log_session('Rysmen', 'testowe')
-ids = gpx_ids(run_log_session, workout_ids(run_log_session, get_num_of_pages(run_log_session)))
-#ids = gpx_ids(run_log_session, workout_ids(run_log_session, 1))
+#ids = gpx_ids(run_log_session, workout_ids(run_log_session, get_num_of_pages(run_log_session)))
+ids = gpx_ids(run_log_session, workout_ids(run_log_session, 1))
 download_gpxies(run_log_session, ids)
