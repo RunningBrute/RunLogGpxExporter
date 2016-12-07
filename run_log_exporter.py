07170cd4667f0531cc553a6a8803e33540efcc3a
salwator <salwator@gmail.com>
2016-12-07 15:54:40 +0100

    refactor

--- run_log_exporter.py
+++ run_log_exporter.py
@@ -2,6 +2,7 @@ import re
 import requests
 from itertools import chain
 
+
 def open_run_log_session(user, passwd):
     run_log_session = requests.session()
     run_log_session.post('https://run-log.com/account/login/', data={'username': user,
@@ -23,7 +24,6 @@ def workout_ids(session, num_of_pages):
     def extract_id(s):
         return int(s.replace('show_workout(', ''))
     def ids_from_page(page_num):
-        print(page_num)
         page_code = session.get('https://run-log.com/training/list?page={}'.format(page_num))
         workouts = re.findall('show_workout\(\d+', page_code.text)
         return [extract_id(workout_str) for workout_str in workouts]
@@ -36,7 +36,7 @@ def gpx_ids(session, workouts):
     k = []
     for count, workout_id in enumerate(workouts):
         q = session.get('https://run-log.com/workout/workout_show/{}'.format(workout_id))
-        print('{}/{}'.format(count, len(workouts)))
+        print('{}/{}'.format(count + 1, len(workouts)))
         try:
             z = re.findall('wt_id&quot;: \d+', q.text)[0]
             c = re.findall('Data:</span><span class="value">\d+-\d+-\d+', q.text)[0]
@@ -47,16 +47,25 @@ def gpx_ids(session, workouts):
     return k
 
 
-def download_gpxies(ids):
+def save_gpx(gpx, id, day):
+    with open("dupa/{}_{}.gpx".format(day, id), "w") as f:
+        f.write(gpx)
+
+
+def download_gpxies(session, ids):
+    def get_gpx(id):
+        return session.get('https://run-log.com/tracks/export_workout_track/Rysmen/{}/gpx'.format(id)).text
+    def correct_dates(gpx):
+        return re.sub('\d+-\d+-\d+', day, gpx)
+    def fill_activity_type(gpx):
+        return re.sub('<trk><trkseg>', '<trk><type>RUNNING</type><trkseg>', gpx)
     for count, (id, day) in enumerate(ids):
-        r = run_log_session.get('https://run-log.com/tracks/export_workout_track/Rysmen/{}/gpx'.format(id))
-        a = re.sub('\d+-\d+-\d+', day, r.text)
-        a = re.sub('<trk><trkseg>', '<trk><type>RUNNING</type><trkseg>', a)
-        with open("dupa/{}_{}.gpx".format(day, id), "w") as f:
-            print('{}/{}'.format(count, len(k)))
-            f.write(a)
+        gpx = fill_activity_type(correct_dates(get_gpx(id)))
+        save_gpx(gpx, id, day)
+        print("{}/{}".format(count + 1, len(ids)))
 
 
 run_log_session = open_run_log_session('Rysmen', 'testowe')
-k = gpx_ids(run_log_session, workout_ids(run_log_session, get_num_of_pages(run_log_session)))
-download_gpxies(k)
+ids = gpx_ids(run_log_session, workout_ids(run_log_session, get_num_of_pages(run_log_session)))
+#ids = gpx_ids(run_log_session, workout_ids(run_log_session, 1))
+download_gpxies(run_log_session, ids)
